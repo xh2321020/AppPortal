5f065789fc22d4e5cbcf4f9ba708817b295fe471
kingsinsd <kingsinsdSunny@163.com>
2016-07-11 12:45:19 +0800

    new personalpage js

---
+++ assets/src/personalpage/contact.js
@@ -0,0 +1,140 @@
+/**
+ * Created by kingsinsd on 2016/7/8.
+ */
+$(document).ready(function () {
+    $.ajax({
+        type: "get",
+        dataType: "json",
+        url: getDepartment,
+        success: function (data, state, jqxhr) {
+            var source =
+            {
+                datatype: "json",
+                datafields: [
+                    {name: 'ou'},
+                    {name: 'pid'},
+                    {name: 'name'},
+                    {name: 'id'}
+                ],
+                id: 'ou',
+                localdata: data
+            };
+
+            // create data adapter.
+            var dataAdapter = new $.jqx.dataAdapter(source);
+            // perform Data Binding.
+            dataAdapter.dataBind();
+            // get the tree items. The first parameter is the item's id. The second parameter is the parent item's id. The 'items' parameter represents
+            // the sub items collection name. Each jqxTree item has a 'label' property, but in the JSON data, we have a 'text' field. The last parameter
+            // specifies the mapping between the 'text' and 'label' fields.
+            var records = dataAdapter.getRecordsHierarchy('ou', 'pid', 'items', [{
+                name: 'name',
+                map: 'label'
+            }]);
+            $('#jqxWidget').jqxTree({source: records, width: '300px'});
+            $('#jqxWidget').on('select', function (event) {
+                var args = event.args;
+                var item = $('#jqxWidget').jqxTree('getItem', args.element);
+                getMembers(item.id);
+            });
+            var ou = "02";
+            getMembers(ou);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+    function getMembers(parmar) {
+        $.ajax({
+            type: "post",
+            contentType: "application/json",
+            data: "",
+            url: getMember + parmar,
+            success: function (data, state, jqxhr) {
+                var leaderHtml = '<div><span>领导/负责人</span><img style="width: 85%;height: 1px;" class="img " '+
+                    'src="assets/images/personalpage/u225_line.png" alt="u225_line"/></div><div style="float: left;width: 100%;margin-top: 5px;">';
+                var allMemberHtml = '<div style="margin-top: 130px;"><span>所有员工</span>'+
+                    '<img id="u225_line" style="width: 86%;height: 1px;" class="img" src="assets/images/personalpage/u225_line.png" alt="u225_line"/>'+
+                    '</div><div style="float: left;margin-top: 5px;">';
+                var countLeaders=0;
+                var countMembers=0;
+                for (var i = 0; i < data.length; i++) {
+                    var images = "";
+                    var mobileNo = "";
+                    var mailAdd = "";
+                    var department = "";
+                    if ((typeof(data[i].mobile) == "undefined") || (typeof(data[i].mobile) == "")) {
+                        mobileNo = "";
+                    } else {
+                        mobileNo = data[i].mobile;
+                    }
+                    if ((typeof(data[i].telephoneNumber) == "undefined") || (typeof(data[i].telephoneNumber) == "")) {
+                        mobileNo = mobileNo + "";
+                    } else {
+                        mobileNo = mobileNo + " ,分机号" + data[i].telephoneNumber;
+                    }
+                    if ((typeof(data[i].imageurl) == "undefined") || (typeof(data[i].imageurl) == "")) {
+                        images = "assets/images/personalpage/defaultUserPhoto.png";
+                    } else {
+                        images = data[i].imageurl;
+                    }
+                    if ((typeof(data[i].mail) == "undefined") || (typeof(data[i].mail) == "")) {
+                        mailAdd = "";
+                    } else {
+                        mailAdd = data[i].mail;
+                    }
+                    for (var j = 1; j < data[i].orgtree.length; j++) {
+                        for (var key in data[i].orgtree[j]) {
+                            if(j<=3){
+                                department = department + '<span>' + data[i].orgtree[j][key] + '</span><br>';
+                            }
+                        }
+                    }
+                    if (data[i].isleader == "1") {
+                        countLeaders ++;
+                        if ((typeof(data[i].userstatus) == "undefined") || (data[i].userstatus == "")) {
+                            leaderHtml = leaderHtml + '<div class="emp-class" onclick="myFunction(\''+data[i].displayname+'\')"><img class="img emp-class-img" src="' + images + '"/>' +
+                            '<div class="emp-class-div"><p><span class="emp-class-div-p-span">' + data[i].displayname +
+                            '</span><br>' + department + '<span>' + mailAdd + '</span><br><span>' + mobileNo +
+                            '</span></p></div></div></a>';
+                        } else {
+                            leaderHtml = leaderHtml + '<div class="emp-class"><img class="img emp-class-img" src="' + images + '"/>' +
+                            '<div class="emp-class-div"><p><span class="emp-class-div-s">授权信息</span><span class="emp-class-div-p-span">' + data[i].displayname +
+                            '</span><br>' + department + '<span>' + mailAdd + '</span><br><span>' + mobileNo +
+                            '</span></p></div></div>';
+                        }
+                    }else{
+                        countMembers ++
+                        if ((typeof(data[i].userstatus) == "undefined") || (data[i].userstatus == "")) {
+                            allMemberHtml = allMemberHtml + '<div class="emp-class"><img class="img emp-class-img" src="' + images + '"/>' +
+                            '<div class="emp-class-div"><p><span class="emp-class-div-p-span">' + data[i].displayname +
+                            '</span><br>' + department + '<span>' + mailAdd + '</span><br><span>' + mobileNo +
+                            '</span></p></div></div>';
+                        } else {
+                            allMemberHtml = allMemberHtml + '<div class="emp-class"><img class="img emp-class-img" src="' + images + '"/>' +
+                            '<div class="emp-class-div"><p><span class="emp-class-div-s">授权信息</span><span class="emp-class-div-p-span">' + data[i].displayname +
+                            '</span><br>' + department + '<span>' + mailAdd + '</span><br><span>' + mobileNo +
+                            '</span></p></div></div>';
+                        }
+                    }
+                }
+                if(countLeaders>0){
+                    $("#leaderArea").html(leaderHtml+'</div>');
+                }
+                if(countMembers>0){
+                    $("#member").html(allMemberHtml+'</div>');
+                }
+                if((countLeaders=0)&&(countMembers=0)){
+                    $("#leaderArea").html("暂无相关人员信息！");
+                    $("#member").html("");
+                }
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    };
+    function myFunction(parmare){
+        alert(parmare);
+    }
+});
---
+++ assets/src/personalpage/contactMembers.js
@@ -0,0 +1,286 @@
+/**
+ * Created by kingsinsd on 2016/7/8.
+ */
+var currentUserId="20116636";
+var pageUserUid="";
+//alert(window.pageUserUid);
+var cancelText = "";
+function editWork(parmar){
+    var span = "#span"+parmar;
+    var input = "#input"+parmar;
+    var btn = "#btn"+parmar;
+    var buttonDiv = "#button"+parmar;
+    var saveBtn = "#edit"+parmar+"Save";
+    var cancelBtn = "#edit"+parmar+"Cancel";
+    $(span).hide();
+    $(input).show();
+    $(btn).hide();
+    cancelText = $(span).text();
+    $(input).val($(span).text());
+    $(buttonDiv).show();
+    $(saveBtn).show();
+    $(cancelBtn).show();
+}
+function editCancel(parmar){
+    var buttonDiv = "#button"+parmar;
+    var input = "#input"+parmar;
+    var span = "#span"+parmar;
+    var btn = "#btn"+parmar;
+    $(buttonDiv).hide();
+    $(input).hide();
+    $(span).show();
+    $(span).text(cancelText);
+    $(btn).show();
+}
+function editSave(parmar,parmarKey){
+    var input = "#input"+parmar;
+    $.ajax({
+        type: "post",
+        contentType: "application/json",
+        data:"" ,
+        url: updateUserProfile + pageUserUid +'&'+parmarKey+'=' + $(input).val(),
+        success: function (data, state, jqxhr) {
+            if(data == "1"){
+                var buttonDiv = "#button"+parmar;
+                var span = "#span"+parmar;
+                var btn = "#btn"+parmar;
+                $(buttonDiv).hide();
+                $(input).hide();
+                $(span).show();
+                //alert($(input).val() + span);
+                $(span).text($(input).val());
+                $(btn).show();
+                alert("保存成功！");
+            }
+        },
+        error: function (err) {
+            console.log(err);
+            //editCancel(parmar);
+        }
+    });
+}
+$(document).ready(function () {
+    function getQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]);
+        return null;
+    }
+    window.pageUserUid=getQueryString("uid");
+    window.pageUserUid = "20116636";
+    var pageUser=getQueryString("uid");
+    pageUser="20116636";
+    var currentUser=window.currentUserId;
+    var sameOrgOu = "";
+    var sameOrgName="";
+    $.ajax({
+        type: "post",
+        contentType: "application/json",
+        data: "",
+        url: getMemberDetails + pageUser,
+        success: function (data, state, jqxhr) {
+            var countLeaders=0;
+            var countMembers=0;
+            //var emplyeeId = "";
+            for (var i = 0; i < data.length; i++) {
+                var iconImages = "";
+                var department = "";
+                if ((typeof(data[i].imageurl) == "undefined") || (typeof(data[i].imageurl) == "")) {
+                    iconImages = "assets/images/personalpage/defaultUserPhoto.png";
+                } else {
+                    iconImages = data[i].imageurl;
+                }
+                for (var j = 1; j < data[i].orgtree.length; j++) {
+                    for (var key in data[i].orgtree[j]) {
+                        sameOrgOu = key;
+                        sameOrgName = data[i].orgtree[j][key];
+                        if(j == "1"){
+                            department = department + '<span>' + sameOrgName + '</span><br>'
+                        }else{
+                            department = department + '<span class="ma-detail-title-span-suo">' + sameOrgName + '</span><br>'
+                        }
+                    }
+                }
+                var leaderHtml = '<img class="img " style="margin-left: 0.5rem;width: 80%;" src="assets/images/personalpage/u291_line.png" alt="u291_line"/>';
+                leaderHtml = leaderHtml +'<div class="xc-main-image"> <img class="img xc-main-image-img" src="'+ iconImages +
+                '" alt="u229"/><p class="xc-main-image-p"><span> ' + data[i].displayname  + '</span></p></div><div class="ma-detail-suo">'+
+                '<div class="ma-detail-suo-org"><div class="ma-detail-title-suo">组&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;织：</div><div class="ma-detail-suo-org-title">'+department+'</div>';
+                var titleArray = ["Title","Tel","Mob","Mail","Loc","Work","Anno","Date"];
+                var keyArray = ["title","telephonenumber","mobile","mail","physicalDeliveryOfficeName","workitem","delegateannu","delegatedate"];
+                var titleCharArray = ["职&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;务：","电&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;话：","手&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;机：","邮&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;件：","办公地点：","工作事项：","委托声明：","委托时间："];
+                //var subHtml=;
+                var interHtml="";
+                for(var k = 0; k< titleArray.length; k++){
+                    var keys = keyArray[k];
+                    if(currentUser == pageUser){
+                        interHtml = interHtml + '<div class="xc-member-data-div"><div class="ma-detail-title-suo">'
+                        + titleCharArray[k]+'</div><div class="xc-member-data">'+
+                        '<span id="span'+titleArray[k]+'" class="xc-workitemsdiv">'+getValue(data[i][keys])+'</span>'+
+                        '<input id="input'+titleArray[k]+'" type="text" class="xc-workitems-input"/></div>'+
+                        '<div id="button'+titleArray[k]+'" class="xc-edit-button-div">'+
+                        '<button onclick="editSave(\''+titleArray[k]+'\',\''+keyArray[k]+'\');" id="edit'+titleArray[k]+
+                        'Save" style="float: right;" class="btn btn-default xc-edit-btn-div xc-edit-btn">保存</button>'+
+                        '<button onclick="editCancel(\''+titleArray[k]+'\');" id="edit'+titleArray[k]+
+                        'Cancel" style="float: right;" class="btn btn-default xc-edit-btn-div xc-edit-btn">取消</button></div>'+
+                        '<button type="button" class="btn btn-default xc-edit-btn-div" id="btn'+titleArray[k]+
+                        '" onclick="editWork(\''+titleArray[k]+
+                        '\')"><span class="glyphicon glyphicon-edit xc-edit-btn-char"></span></button></div>';
+                    }else{
+                        interHtml = interHtml + '<div class="xc-member-data-div"><div class="ma-detail-title-suo">'
+                        + titleCharArray[k]+'</div><div class="xc-member-data">'+
+                        '<span id="span'+titleArray[k]+'" class="xc-workitemsdiv">'+getValue(data[i][keys])+'</span>'+
+                        '<input id="input'+titleArray[k]+'" type="text" class="xc-workitems-input"/></div>';
+                    }
+                }
+                $("#userInfo").html(leaderHtml+interHtml+"</div>");
+            }
+            getChain(pageUser);
+            getXinCheng(pageUser);
+            getSameOrg(sameOrgOu);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+    function getValue(key){
+        if ((typeof(key) == "undefined") || (typeof(key) == "")) {
+            return  "";
+        } else {
+            return  key;
+        }
+    }
+    function getChain(parmar){
+        $.ajax({
+            type: "get",
+            dataType:"json",
+            url: getChainsDetails + parmar,
+            success: function (data, state, jqxhr) {
+                var leveArray = new Array();
+                var titleArray = new Array();
+                var displaynames="";
+                var currentposition = 0;
+                if(data.length>0){
+                    currentposition = data[0].position;
+                }
+                for (var i = 0; i < data.length; i++) {
+                    if(data[i].position == currentposition){
+                        var member = leveArray[currentposition-1];
+                        var currentMember = member + data[i].displayname+",";
+                        displaynames = data[i].displayname;
+                        leveArray[currentposition-1] = currentMember;
+                        for (var m = 2; m < data[i].orgtree.length; m++) {
+                            for (var key in data[i].orgtree[m]) {
+                                titleArray[currentposition-1] = data[i].orgtree[m][key];
+                            }
+                        }
+                    }else{
+                        currentposition = data[i].position;
+                        var member = leveArray[currentposition-1];
+                        var currentMember = member + data[i].displayname+",";
+                        displaynames = data[i].displayname;
+                        leveArray[currentposition-1] = currentMember;
+                        for (var m = 2; m < data[i].orgtree.length; m++) {
+                            for (var key in data[i].orgtree[m]) {
+                                titleArray[currentposition-1] = data[i].orgtree[m][key];
+                            }
+                        }
+                    }
+                }
+                var firstManageUrl = "";
+                for(var i = 0; i< titleArray.length; i++){
+                    titleArray[i] = titleArray[i].replace(/undefined/, "");
+                }
+                for(var i = 0; i< leveArray.length; i++){
+                    leveArray[i] = leveArray[i].replace(/undefined/, "");
+                    leveArray[i] = leveArray[i].substring(0,leveArray[i].length-1);
+                    if(i == "0"){
+                        firstManageUrl = '<div  style="width: 100%; text-align: center" style="background-color: red;"><span class="right-bottom-org-sp">'+titleArray[i]+
+                        '</span><div class="right-bottom-org-sp-split" style="border: 0;">'+leveArray[i]+'</div></div>';
+                    }else{
+                        firstManageUrl = '<div  style="width: 100%; text-align: center;"><span class="right-bottom-org-sp">'+titleArray[i]+
+                        '</span><div class="right-bottom-org-sp-split">'+leveArray[i]+'</div>'+
+                        '<img id="u393" class="img kh-xz" src="assets/images/personalpage/u393.png" alt="u393"/></div>' + firstManageUrl;
+                    }
+                }
+                var lineHtml = '<img id="u291_line" style="margin-left: 5px;width: 80%;height: 1px; vertical-align: top;" src="assets/images/personalpage/u291_line.png"/>';
+                var line='<div class="container-div clear-float"><label for="exampleInputPassword1" class="title-label">汇报链</label><div style="width: 100%;">'+ lineHtml;
+                $("#userChains").html(line + firstManageUrl + '</div></div>');
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    }
+    function getWeeks(parmar){
+        var oDate1 = new Date(parmar);
+        var arrayChar=["日","一","二","三","四","五","六"];
+        var dataChar = parmar.substring(parmar.length-2, parmar.length);
+        var weekDay = dataChar+arrayChar[new Date(oDate1).getDay()];
+        var monthChar = parmar.substring(5, parmar.length-3)+"月";
+        if(monthChar.indexOf("0") == 0){
+            monthChar = monthChar.substring(1, monthChar.length);
+        }
+        var weeksArr = [weekDay,monthChar];
+        return weeksArr;
+    }
+    function getXinCheng(parmar){
+        parmar = "10086"
+        $.ajax({
+            type: "get",
+            dataType:"json",
+            url: getXingChengDetails + parmar,
+            success: function (data, state, jqxhr) {
+                var xinChengIcon = "";
+                var xinChengDetail=""
+                for(var i = 0; i< data.length; i++){
+                    var arrayChar=getWeeks(data[i].startdate);
+                    xinChengIcon = xinChengIcon + '<div class="xc-div"><div class="xc-div-datediv">'+
+                    '<div class="round-data">'+arrayChar[0]+'</div><p class="right-p">'+arrayChar[1]+'</p></div><div>'+
+                    '<span class="xc-right-name" style="max-width: 18rem;" title="'+data[i].description+'">'+ data[i].description+'</span><br>'+
+                    '<span class="right-bottom-org">'+data[i].starttime+'~'+data[i].endtime+' 领导活动</span></div></div>';
+                }
+                $("#xinChengDate").html(xinChengIcon + xinChengIcon);
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    }
+    function getSameOrg(pramar){
+        $.ajax({
+            type: "get",
+            dataType:"json",
+            url: getSameOrgDetails+pramar,
+            success: function (data, state, jqxhr) {
+                var lineHtml = '<img id="u291_line" class="img " style="margin-left: 5px;width: 80%;height: 1px;"'+
+                    'src="assets/images/personalpage/u291_line.png" alt="u291_line"/>';
+                var sameOrgHtml = "";
+                var sameOrgCount = "";
+                var moreHtml = "";
+                if(data.length < 4){
+                    sameOrgCount=data.length;
+                }else{
+                    sameOrgCount=4;
+                    moreHtml = '<div style="margin-right: 5px; background-color: red; min-height: 4.7rem;"><a href="#" style="float:right; margin-top:3rem;">More>></a></div>';
+                }
+                for(var i = 0; i< sameOrgCount; i++){
+                    var iconImages=""
+                    if ((typeof(data[i].imageurl) == "undefined") || (typeof(data[i].imageurl) == "")) {
+                        iconImages = "assets/images/personalpage/defaultUserPhoto.png";
+                    } else {
+                        iconImages = data[i].imageurl;
+                    }
+                    sameOrgHtml = sameOrgHtml +'<div style="margin-top: 5px;"><div style="float: left;"><div style="margin-right: 5px;">'+
+                    '<img class="img right-bottom-img" src="'+iconImages+'" alt="u280"/></div></div>'+
+                    '<span class="right-name">'+data[i].displayname+'</span><br>' +
+                    '<span class="right-bottom-org">'+ sameOrgName + '</span></div>';
+                }
+                var sameOrgMemberDiv = '<div class="container-div clear-float"><label class="title-label">同一组织</label><div style="width: 100%;" >';
+                $("#sameOrgMember").html(sameOrgMemberDiv+lineHtml+sameOrgHtml + moreHtml+ '</div></div>');
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    }
+});
