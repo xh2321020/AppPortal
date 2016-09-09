ce76238ce2601beb801da550d51da9f9d796cf37
caniceee <384393456@qq.com>
2016-09-09 16:48:29 -0400

    远程投稿修改20160910： 1、远程投稿修改； 2、门户管理修改；

--- src/portal/news_publish.js
+++ src/portal/news_publish.js
@@ -1,12 +1,15 @@
-var cloumnList = [];
 var newsDetil = {};
+var cloumnList = [];
+var localColumnList = [];
 var curCloumnId = "";
+var curLocalCloumnId = "";
 
 var newsApp =  new Vue({
     el: '#newsApp',
     data: {
-        cloumnList: cloumnList,
         newsDetil: newsDetil,
+        cloumnList: cloumnList,
+        localColumnList: localColumnList,
     },
     methods: {
     },
@@ -32,10 +35,11 @@ function obj2str(o){
     return o.toString();
 }
 
-function cloumnClick(self){
+localCloumnClick
+function localCloumnClick(self){
     var index = $(self).index();
-    curCloumnId = newsApp.cloumnList[index].id;
-    $(".ul-org li").each(function () {
+    curLocalCloumnId = newsApp.localColumnList[index].id;
+    $("#content2 .ul-org li").each(function () {
         var curIndex = $(this).index();
         if(index == curIndex){
             $(this).find("span").removeClass("span-org");
@@ -47,9 +51,27 @@ function cloumnClick(self){
     });
 };
 
-function publish(){
-    newsApp.newsDetil.cat_id = curCloumnId;
+function cloumnClick(self){
+    var index = $(self).index();
+    curCloumnId = newsApp.cloumnList[index].id;
+    $("#content .ul-org li").each(function () {
+        var curIndex = $(this).index();
+        if(index == curIndex){
+            $(this).find("span").removeClass("span-org");
+            $(this).find("span").addClass("span-org-selected");
+        } else{
+            $(this).find("span").removeClass("span-org-selected");
+            $(this).find("span").addClass("span-org");
+        }
+    });
+};
+
+function publish2Local(){
+    newsApp.newsDetil.cat_id = curLocalCloumnId;
     newsApp.newsDetil.att = newsApp.newsDetil.attachments;
+    for(var i = 0, j = newsApp.newsDetil.att.length; i < j; i++){
+        newsApp.newsDetil.att[i].version = "1";
+    }
     $.ajax({
         type: "post",
         dataType: "json",
@@ -57,6 +79,7 @@ function publish(){
         contentType:'application/json',
         url: 'http://172.16.51.137:8010/api/news/add?apikey=e71982d5401b488da4acef8827c41845',
         success: function (data) {
+            console.log("success************=");
             $.blockUI({
                 message: '祝贺你，投稿成功！',
                 timeout: 1200,
@@ -72,6 +95,7 @@ function publish(){
             });
         },
         error: function (err) {
+            console.log("error************=");
             $.blockUI({
                 message: '很遗憾，投稿失败！',
                 timeout: 1200,
@@ -89,12 +113,82 @@ function publish(){
     });
 };
 
-var getCloumnList = function(){
+function publish2Hq(){
+    newsApp.newsDetil.cat_id = curCloumnId;
+    newsApp.newsDetil.att = newsApp.newsDetil.attachments;
+    for(var i = 0, j = newsApp.newsDetil.att.length; i < j; i++){
+        newsApp.newsDetil.att[i].version = "1";
+    }
+    $.ajax({
+        type: "post",
+        dataType: "json",
+        data: obj2str(newsApp.newsDetil),
+        contentType:'application/json',
+        url: 'http://172.16.51.137:8010/api/news/add?apikey=e71982d5401b488da4acef8827c41845',
+        success: function (data) {
+            console.log("success************=");
+            $.blockUI({
+                message: '祝贺你，投稿成功！',
+                timeout: 1200,
+                css: {
+                    border: 'none',
+                    padding: '15px',
+                    backgroundColor: '#000',
+                    '-webkit-border-radius': '10px',
+                    '-moz-border-radius': '10px',
+                    opacity: .5,
+                    color: '#fff'
+                }
+            });
+        },
+        error: function (err) {
+            console.log("error************=");
+            $.blockUI({
+                message: '很遗憾，投稿失败！',
+                timeout: 1200,
+                css: {
+                    border: 'none',
+                    padding: '15px',
+                    backgroundColor: '#000',
+                    '-webkit-border-radius': '10px',
+                    '-moz-border-radius': '10px',
+                    opacity: .5,
+                    color: '#fff'
+                }
+            });
+        }
+    });
+};
+
+var getLoaclCloumnList = function(cloumnId){
     $.ajax({
         type: "get",
         dataType: "json",
         contentType:'application/json; charset=utf-8;',
-        url: 'http://10.15.251.110:8010/api/homepage/homepageform/?hpid=100&apikey=a16cb0c916404be78cb0805fefc7d26a',
+        url: 'http://10.15.251.110:8010/api/homepage/homepageform/?hpid=' + cloumnId + '&apikey=a16cb0c916404be78cb0805fefc7d26a',
+        success: function (data) {
+            newsApp.localColumnList = data;
+            for(var i = 0, j = newsApp.localColumnList.length; i < j; i++){
+                if(i == 0){
+                    curLocalCloumnId = newsApp.localColumnList[i].id;
+                    newsApp.localColumnList[i].isSelected = true;
+                } else{
+                    newsApp.localColumnList[i].isSelected = false;
+                }
+            }
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+};
+
+var getCloumnList = function(cloumnId){
+    $.ajax({
+        type: "get",
+        dataType: "json",
+        contentType:'application/json; charset=utf-8;',
+        url: 'http://10.15.251.110:8010/api/homepage/homepageform/?hpid=' + cloumnId + '&apikey=a16cb0c916404be78cb0805fefc7d26a',
         success: function (data) {
             newsApp.cloumnList = data;
             for(var i = 0, j = newsApp.cloumnList.length; i < j; i++){
@@ -140,4 +234,10 @@ if(id && id.length > 0){
     getNewsDetail('7493');
 }
 
-getCloumnList();
+var cloumnId = getUrlParamsString('cloumnId');
+if(cloumnId && cloumnId.length > 0){
+    getLoaclCloumnList(cloumnId);
+} else{
+    getLoaclCloumnList('100');
+}
+getCloumnList('100');
